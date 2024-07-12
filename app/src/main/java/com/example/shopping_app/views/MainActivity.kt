6ee17a9ce0eab6acc469d23911f9e5e9caa10f0c
shabeen <Shabeen@infobreez.com>
2024-07-12 12:06:31 +0530

    - Fixes back press issue from cart screen

--- app/src/main/java/com/example/shopping_app/views/MainActivity.kt
+++ app/src/main/java/com/example/shopping_app/views/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.shopping_app.views
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.shopping_app.R
 import com.example.shopping_app.databinding.ActivityMainBinding
@@ -32,17 +34,31 @@ class MainActivity : AppCompatActivity() {
         // setup navigation
         setUpNavigation(activityMainBinding.toolbar)
 
+        
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+               if (navController.currentDestination?.id == R.id.shopFragment){
+                   finish()
+               } else {
+                   navController.navigateUp()
+               }
+            }
+        })
+
     }
 
     private fun setUpNavigation(toolbar: Toolbar) {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        navController = navHostFragment.navController
+        navController = navHostFragment.findNavController()
         NavigationUI.setupWithNavController(toolbar, navController)
     }
 
+    // handle back press
     override fun onSupportNavigateUp(): Boolean {
-        navController.navigateUp()
+        if (navController.currentDestination?.id != R.id.shopFragment){
+            navController.navigateUp()
+        }
         return super.onSupportNavigateUp()
     }
 
